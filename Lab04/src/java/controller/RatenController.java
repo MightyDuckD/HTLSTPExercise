8ce34be5716478dc0ad92a1cb4db35fdfcaf87c9
Simon Lehner-D <simon.lehnerd@gmail.com>
2016-11-20 00:35:17 +0100

    Minor refactoring

--- Lab04/src/java/controller/RatenController.java
+++ Lab04/src/java/controller/RatenController.java
@@ -11,7 +11,6 @@ import java.util.List;
 import java.util.Random;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
-import javax.faces.bean.ManagedBean;
 import javax.faces.event.ActionEvent;
 import javax.inject.Named;
 
