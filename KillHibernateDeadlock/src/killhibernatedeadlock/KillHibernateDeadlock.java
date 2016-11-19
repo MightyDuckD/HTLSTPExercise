bfe5c9d964e328be125413091d84df85a8856925
Simon Lehner-D <simon.lehnerd@gmail.com>
2016-11-19 14:51:41 +0100

    Add License and cleanup

--- KillHibernateDeadlock/src/killhibernatedeadlock/KillHibernateDeadlock.java
+++ KillHibernateDeadlock/src/killhibernatedeadlock/KillHibernateDeadlock.java
@@ -7,23 +7,42 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (c) 2016 Simon Lehner-Dittenberger
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
  */
+ 
 /**
  *
- * @author Simon
+ * @author Simon Lehner-Dittenberger <simon.lehnerd@gmail.com>
  */
 public class KillHibernateDeadlock {
 
     // JDBC driver name and database URL
-    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String JDBC_DRIVER = "org.postgresql.Driver";
     static final String DB_URL = "jdbc:postgresql://localhost:5433/";
     static final String DB = "5A_10_Lab3";
     //  Database credentials
     static final String USER = "5AHIF_16";
-    static final String PASS = "jeke18";
+    static final String PASS = "*******";
 
     public static void main(String args[]) throws ClassNotFoundException, SQLException {
         String query = ""
@@ -42,7 +61,7 @@ public class KillHibernateDeadlock {
         Connection conn = null;
         Statement stmt = null;
         //STEP 2: Register JDBC driver
-        Class.forName("org.postgresql.Driver");
+        Class.forName(JDBC_DRIVER);
 
         //STEP 3: Open a connection
         System.out.println("Connecting to database...");
