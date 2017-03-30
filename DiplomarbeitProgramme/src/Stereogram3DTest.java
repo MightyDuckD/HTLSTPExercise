27ddd85765b0c400069fd56166974f35713559bb
Simon Lehner-D. <simon.lehnerd@gmail.com>
2017-03-30 02:19:54 +0200

    Really good results

--- DiplomarbeitProgramme/src/Stereogram3DTest.java
+++ DiplomarbeitProgramme/src/Stereogram3DTest.java
@@ -71,6 +71,14 @@ public class Stereogram3DTest {
         return data;
     }
 
+    public static BufferedImage avgbox(BufferedImage input, int size) {
+        float data[] = new float[size * size];
+        for (int i = 0; i < data.length; i++) {
+            data[i] = 1f / (size * size);
+        }
+        return new ConvolveOp(new Kernel(size, size, data)).filter(input, null);
+    }
+
     public static BufferedImage gaussian(BufferedImage input) {
         float data[] = scale(new float[]{
             1, 4, 6, 4, 1,
@@ -80,6 +88,48 @@ public class Stereogram3DTest {
             1, 4, 6, 4, 1
         }, 1 / 256.0f);
         return new ConvolveOp(new Kernel(5, 5, data)).filter(input, null);
+    }
+
+    public static BufferedImage oilpainting(BufferedImage input, int levelcount, int rad) {
+        BufferedImage result = convert(input, input.getType());
+        int[][] level = new int[input.getWidth()][input.getHeight()];
+        for (int x = 0; x < input.getWidth(); x++) {
+            for (int y = 0; y < input.getHeight(); y++) {
+                level[x][y] = (avg(input.getRGB(x, y)) * levelcount / 255);
+            }
+        }
+
+        for (int x = rad; x < input.getWidth() - rad; x++) {
+            for (int y = rad; y < input.getHeight() - rad; y++) {
+                int[] r = new int[levelcount+1];
+                int[] g = new int[levelcount+1];
+                int[] b = new int[levelcount+1];
+                int[] cnt = new int[levelcount+1];
+                for (int i = -rad; i <= rad; i++) {
+                    for (int j = -rad; j <= rad; j++) {
+                        int current = level[x + i][y + j];
+                        int color = input.getRGB(x + i, y + j);
+                        r[current] += r(color);
+                        g[current] += g(color);
+                        b[current] += b(color);
+                        cnt[current]++;
+                    }
+                }
+                int maxi = 0;
+                for (int i = 0; i <= levelcount; i++) {
+                    if (cnt[maxi] < cnt[i]) {
+                        maxi = i;
+                    }
+                }
+                result.setRGB(x, y, rgb(
+                        r[maxi] / cnt[maxi],
+                        g[maxi] / cnt[maxi],
+                        b[maxi] / cnt[maxi]
+                ));
+            }
+        }
+
+        return result;
     }
 
     public static BufferedImage diff(BufferedImage source, int offset) {
@@ -92,21 +142,16 @@ public class Stereogram3DTest {
         return img;
     }
 
-    public static void main(String[] args) throws Exception {
-//        BufferedImage original = convert(ImageIO.read(new URL("http://localhost:8080/geoui/res/what.png")), BufferedImage.TYPE_INT_RGB);
-        BufferedImage original = convert(ImageIO.read(new URL("http://localhost:8080/geoui/res/city.jpg")), BufferedImage.TYPE_INT_RGB);
-        BufferedImage result = new BufferedImage(original.getWidth(), original.getHeight(), BufferedImage.TYPE_INT_RGB);
-        int tresh = 10;
+    
+    public static void doit(BufferedImage original, BufferedImage result, int tresh) {
         int max = 630;
         int min = 530;
         for (int i = max; i >= min; i--) {
 //        for (int i = min; i <= max; i++) {
 //            BufferedImage other = gaussian(gaussian(diff(original, i)));
-            BufferedImage other = gaussian(diff(original, i));
-//BufferedImage other = diff(original, i);
-            if(i == 250)
-                ImageIO.write(other,"png",new File("output.gaus.png"));
-            Color color = new Color((float)(i-min) / (max-min), 0.3f, 0.3f);
+//            BufferedImage other = gaussian(diff(original, i));
+            BufferedImage other = diff(original, i);
+            Color color = new Color((float) (i - min) / (max - min), 0.3f, 0.3f);
             for (int x = i; x < result.getWidth(); x++) {
                 for (int y = 0; y < result.getHeight(); y++) {
                     if (avg(other.getRGB(x, y)) < tresh) {
@@ -115,6 +160,19 @@ public class Stereogram3DTest {
                 }
             }
         }
+    }
+    public static void main(String[] args) throws Exception {
+//        BufferedImage original = convert(ImageIO.read(new URL("http://localhost:8080/geoui/res/what.png")), BufferedImage.TYPE_INT_RGB);
+        BufferedImage original = convert(ImageIO.read(new URL("http://localhost:8080/geoui/res/city.jpg")), BufferedImage.TYPE_INT_RGB);
+        BufferedImage result = new BufferedImage(original.getWidth(), original.getHeight(), BufferedImage.TYPE_INT_RGB);
+//        ImageIO.write(oilpainting(original, 20,3), "png", new File("output-3d-oil.png"));
+        
+        doit(original, result,20);
+        doit(original, result,5);
+        doit(original, result,2);
+        doit(original, result,1);
+
         ImageIO.write(result, "png", new File("output-3d.png"));
+        ImageIO.write(oilpainting(gaussian(result), 30,5), "png", new File("output-3d-oil.png"));
     }
 }
